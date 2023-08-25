95a7b8a9627f2c9abac84d75d21978019cad626d
jaydabhi2002 <120631587+jaydabhi2002@users.noreply.github.com>
2023-08-25 13:16:15 +0530

    added

--- router/adminrouter.js
+++ router/adminrouter.js
@@ -1,216 +1,225 @@
-const router = require("express").Router()
-const Admin = require("../model/admins")
-const jwt = require("jsonwebtoken")
-const aauth = require("../middleware/adminauth")
+const router = require("express").Router();
+const Admin = require("../model/admins");
+const jwt = require("jsonwebtoken");
+const aauth = require("../middleware/adminauth");
 
-router.get("/admin_dashboard",aauth,(req,resp)=>{
-    resp.render("admin_dashboard")
-})
+router.get("/admin_dashboard", aauth, (req, resp) => {
+  resp.render("admin_dashboard");
+});
 
 // router.get("/Users", (req,resp)=>{
 //     resp.render("users")
 // })
 
-router.get("/Orders", (req,resp)=>{
-    resp.render("Orders")
-})
+router.get("/Orders", (req, resp) => {
+  resp.render("Orders");
+});
 // ----------------------------------------ADMIN LOGIN------------------------------------------------
-router.get("/admin_login", (req,resp)=>{
-    resp.render("admin_login")
-})
-router.post("/do_adminlogin",async(req,resp)=>{
-    try {
-        const admin = await Admin.findOne({uname: req.body.uname})
-        // console.log(admin);
-        if(admin.pass == req.body.pass) 
-        {
-            const token = await jwt.sign({_id : admin._id},process.env.A_KEY)
-            resp.cookie("ajwt",token)
-            resp.redirect("admin_dashboard")
-        }
-        else
-        {
-            resp.render("admin_login",{err:"Invalid credentials !!!"})
-        }
-    } catch (error) {
-        console.log(error);
-        resp.render("admin_login",{err:"Invalid credentials !!!"})
+router.get("/admin_login", (req, resp) => {
+  resp.render("admin_login");
+});
+router.post("/do_adminlogin", async (req, resp) => {
+  try {
+    const admin = await Admin.findOne({ uname: req.body.uname });
+    // console.log(admin);
+    if (admin.pass == req.body.pass) {
+      const token = await jwt.sign({ _id: admin._id }, process.env.A_KEY);
+      resp.cookie("ajwt", token);
+      resp.redirect("admin_dashboard");
+    } else {
+      resp.render("admin_login", { err: "Invalid credentials !!!" });
     }
-})
+  } catch (error) {
+    console.log(error);
+    resp.render("admin_login", { err: "Invalid credentials !!!" });
+  }
+});
 // ------------------------------------ADMIN LOGOUT-------------------------------------------------
 
-router.get("/do_adminlogout",async(req, resp) => {
-    try {
-        resp.clearCookie("ajwt")
-        resp.render("admin_login")
-    } catch (error) {
-        console.log(error);
-    }
-})
+router.get("/do_adminlogout", async (req, resp) => {
+  try {
+    resp.clearCookie("ajwt");
+    resp.render("admin_login");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // -------------------------------------CATEGORY PAGE-----------------------------------------------------
 
-const Category = require("../model/categories")
+const Category = require("../model/categories");
 
-router.get("/Category",async(req,resp)=>{
-    try {
-        const data = await Category.find()
-        // console.log(data);
-        resp.render("admin_category",{catdata:data})
-    } catch (error) {
-        console.log(error);
+router.get("/Category", async (req, resp) => {
+  try {
+    const data = await Category.find();
+    // console.log(data);
+    resp.render("admin_category", { catdata: data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.post("/add_category", aauth, async (req, resp) => {
+  console.log(req.body.id);
+  try {
+    if (req.body.id == "") {
+      const cat = new Category(req.body);
+      await cat.save();
+      console.log(cat);
+      resp.redirect("category");
+    } else {
+      await Category.findByIdAndUpdate(req.body.id, {
+        catname: req.body.catname,
+      });
+      resp.redirect("category");
     }
-})
-router.post("/add_category",aauth,async(req,resp)=>{
-    console.log(req.body.id);
-    try {
-           if(req.body.id==""){
-           const cat = new Category(req.body)
-           await cat.save()
-           console.log(cat);
-           resp.redirect("category")
-           }
-           else
-           {
-           await Category.findByIdAndUpdate(req.body.id,{catname:req.body.catname})
-           resp.redirect("category")
-           }
-    } catch (error) {
-        console.log(error);
-    }
-})
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.get("/editcategory",async(req,resp)=>{
-    try {
-      const _id = req.query.catid
-      const data = await Category.findOne({_id : _id})
-      const catdata = await Category.find()
-      resp.render("admin_category",{edata: data,catdata:catdata})
-    } catch (error) {
-      console.log(error); 
-    }
-  })
+router.get("/editcategory", async (req, resp) => {
+  try {
+    const _id = req.query.catid;
+    const data = await Category.findOne({ _id: _id });
+    const catdata = await Category.find();
+    resp.render("admin_category", { edata: data, catdata: catdata });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-  router.get("/deletecategory",async(req,resp)=>{
-    try {
-      const id = req.query.catid
-      const data =  await Category.findByIdAndDelete(id)
-      resp.redirect("category")
-    } catch (error) {
-      console.log(error); 
-    }
-  })
+router.get("/deletecategory", async (req, resp) => {
+  try {
+    const id = req.query.catid;
+    const data = await Category.findByIdAndDelete(id);
+    resp.redirect("category");
+  } catch (error) {
+    console.log(error);
+  }
+});
 // -------------------------------------------PRODUCT PAGE------------------------------------------------
-const product = require ("../model/product")
-const multer = require("multer")
-const fs = require("fs")
+const product = require("../model/product");
+const multer = require("multer");
+const fs = require("fs");
 
 const storageEngine = multer.diskStorage({
-    destination: "./public/productimg",
-    filename: (req, file, cb) => {
-      cb(null, `${Date.now()}--${file.originalname}`);
-    },
-  });
-  const upload = multer({
-    storage: storageEngine,
-  }); 
+  destination: "./product/productimg",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}--${file.originalname}`);
+  },
+});
+const upload = multer({
+  storage: storageEngine,
+});
 
-router.get("/Product",aauth,async(req,resp)=>{
-    try {
-        const catdata = await Category.find()
+router.get("/Product", aauth, async (req, resp) => {
+  try {
+    const catdata = await Category.find();
 
-        const data = await product.aggregate([{$lookup:{from:"categories",localField:"catid",foreignField:"_id",as:"category"}}])
-        resp.render("admin_product",{pdata:data,catdata:catdata})
-    } catch (error) {
-        console.log(error);
-    }   
-    // resp.render("admin_product")
-})
-router.post("/add_product",upload.single("img"),async(req,resp)=>{
-    
-    try {
-        if(req.body.id=="")
-        {
-            const prod = new product ({
-                catid:req.body.catid,
-                pname:req.body.pname,
-                price:req.body.price,
-                qty:req.body.qty,
-                img:req.file.filename
-               })
-            // console.log(prod);
-                const dt = await prod.save()
-                // console.log(dt);
-                resp.redirect("Product")
-        }
-        else
-        {
-                const dt = await product.findByIdAndUpdate(req.body.id,{
-                catid:req.body.catid,
-                pname:req.body.pname,
-                price:req.body.price,
-                qty:req.body.qty,
-                img:req.file.filename
-               })
-            // console.log(dt);
-                resp.redirect("Product")
-        }
-        
-    } catch (error) {
-        console.log(error);
+    const data = await product.aggregate([
+      {
+        $lookup: {
+          from: "categories",
+          localField: "catid",
+          foreignField: "_id",
+          as: "category",
+        },
+      },
+    ]);
+    resp.render("admin_product", { pdata: data, catdata: catdata });
+  } catch (error) {
+    console.log(error);
+  }
+  // resp.render("admin_product")
+});
+router.post("/add_product", upload.single("img"), async (req, resp) => {
+  try {
+    if (req.body.id == "") {
+      const prod = new product({
+        catid: req.body.catid,
+        pname: req.body.pname,
+        price: req.body.price,
+        qty: req.body.qty,
+        img: req.file.filename,
+      });
+      // console.log(prod);
+      const dt = await prod.save();
+      // console.log(dt);
+      resp.redirect("Product");
+    } else {
+      const dt = await product.findByIdAndUpdate(req.body.id, {
+        catid: req.body.catid,
+        pname: req.body.pname,
+        price: req.body.price,
+        qty: req.body.qty,
+        img: req.file.filename,
+      });
+      // console.log(dt);
+      resp.redirect("Product");
     }
-})
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.get("/deleteproduct",async(req,resp)=>{
-    try {
-        const id = req.query.pid
-        const data = await product.findByIdAndDelete(id)
-        fs.unlinkSync("public/productimg/"+data.img)
-        resp.redirect("product")
-
-    } catch (error) {
-        console.log(error);
-    }
-})
+router.get("/deleteproduct", async (req, resp) => {
+  try {
+    const id = req.query.pid;
+    const data = await product.findByIdAndDelete(id);
+    fs.unlinkSync("public/productimg/" + data.img);
+    resp.redirect("product");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.get("/editproduct", async (req, resp) => {
-    try {
-        const id = req.query.pid
-        const data = await product.findOne({_id:id})
-        const catdata = await Category.find()
-        const prod = await product.aggregate([{$lookup:{from:"categories",localField:"catid",foreignField:"_id",as:"category"}}])
-        resp.render("admin_product",{edata:data,catdata:catdata,pdata:prod})
-        
-    } catch (error) {
-        console.log(error);
-    }
-})
+  try {
+    const id = req.query.pid;
+    const data = await product.findOne({ _id: id });
+    const catdata = await Category.find();
+    const prod = await product.aggregate([
+      {
+        $lookup: {
+          from: "categories",
+          localField: "catid",
+          foreignField: "_id",
+          as: "category",
+        },
+      },
+    ]);
+    resp.render("admin_product", {
+      edata: data,
+      catdata: catdata,
+      pdata: prod,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // -----------------------------------Users--------------------------------------------------------
 
-const User = require("../model/user")
+const User = require("../model/user");
 
-router.get("/viewusers",async(req,resp)=>{
-    try {
-        const user = await User.find()
-        resp.render("users",{userdata:user})
-    } catch (error) {
-        console.log(error);
-    }
-})
+router.get("/viewusers", async (req, resp) => {
+  try {
+    const user = await User.find();
+    resp.render("users", { userdata: user });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // --------------------------------------orders------------------------------------------------------
-const Order = require("../model/orders")
-router.get("/vieworders",async(req,resp)=>{
-    try {
-        const orderdata = await Order.find();
-        resp.render("orders",{orderdata:orderdata})
-    } catch (error) {
-        console.log(error);
-    }
-    
+const Order = require("../model/orders");
+router.get("/vieworders", async (req, resp) => {
+  try {
+    const orderdata = await Order.find();
+    resp.render("orders", { orderdata: orderdata });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-})
-
-
-module.exports=router
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,40 +1,40 @@
-const mongoose = require("mongoose")
-const express = require("express")
-const app = express()
-require("dotenv").config()
-const path = require("path")
-const hbs = require("hbs")
-const bodyParser =require("body-parser")
-const cookieParser =require("cookie-parser")
-const cors = require('cors')
-app.use(cors())
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const path = require("path");
+const hbs = require("hbs");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+app.use(cors());
 
-const PORT=process.env.PORT
-const DB_URL=process.env.DB_URL
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
 
-app.use(express.json())
-mongoose.connect(DB_URL).then(()=>{
-console.log("My DataBase Connted");
-}).catch(error=>{
+app.use(express.json());
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("My DataBase Connted");
+  })
+  .catch((error) => {
     console.log(error);
-})
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: false }))
-const publicpath = path.join(__dirname,"../public")
-const viewpath = path.join(__dirname,"../templetes/views")
-const partialspath = path.join(__dirname,"../templetes/partials")
+  });
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+const publicpath = path.join(__dirname, "../public");
+const viewpath = path.join(__dirname, "../templetes/views");
+const partialspath = path.join(__dirname, "../templetes/partials");
 
-app.set("view engine","hbs")
-app.set("views",viewpath)
-hbs.registerPartials(partialspath)
-app.use(express.static(publicpath))
+app.set("view engine", "hbs");
+app.set("views", viewpath);
+hbs.registerPartials(partialspath);
+app.use(express.static(publicpath));
 
+app.use("/", require("../router/userouter"));
+app.use("/", require("../router/adminrouter"));
 
-
-app.use("/",require("../router/userouter"))
-app.use("/",require("../router/adminrouter"))
-
-
-app.listen(PORT,()=>{
-    console.log("server running on PORT : "+PORT);
-})
+app.listen(PORT, () => {
+  console.log("server running on PORT : " + PORT);
+});
